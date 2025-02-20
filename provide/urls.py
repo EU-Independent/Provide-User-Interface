f2ab9bdf8833f65e135046c0e3489590fc40d102
ritabei <71849396+ritabei@users.noreply.github.com>
2025-02-20 11:51:58 +0200

    Update urls.py

--- provide/urls.py
+++ provide/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import provide_offer
 
 urlpatterns = [
-    path('offer/', provide_offer, name='provide_offer'),
+    path('', provide_offer, name='provide_offer'),
 ]
