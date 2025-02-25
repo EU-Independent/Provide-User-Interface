94b0d1f44737a9537aae7b081e38554fe20c7a4f
Beigaite Rita <rita.beigaite@vtt.fi>
2025-02-25 13:45:04 +0200

    Combines the upload button with url field. Working

--- provide/urls.py
+++ provide/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
    
     path('', views.provide_offer, name='provide_offer'),
-
     path('upload/', views.upload_view, name='file_upload'),  
-
     path('upload/<int:file_id>/', views.upload_view, name='file_download'),  
 ]
